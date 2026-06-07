a58624124bebbead31f1b0f7511091141cf1e959
Symbol Pamnani <symbolrani@gmail.com>
2026-06-07 07:15:25 +0500

    Update db.ts

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log("MONGO_URI =", process.env.MONGO_URI);
+
 const mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/miniticketdb';
 
 export async function connectDatabase(): Promise<void> {
